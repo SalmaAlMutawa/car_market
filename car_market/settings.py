85bd5c3bbb98b203465be558bcf714667574a7e0
SalmaAlMutawa <s.almutawa96@gmail.com>
2018-10-15 10:36:13 +0300

    prepared django project for deployment

--- car_market/settings.py
+++ car_market/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mmj9k6e^e=si_+-lz-505sj(!)-!vly6zy3+xb(89^ldu&wcb9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1' , '178.128.201.152' ]
 
 
 # Application definition
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'car_market.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': '42cb83cc8371c153510ca6eb87fff123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
